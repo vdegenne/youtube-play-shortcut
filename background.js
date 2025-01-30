d4ed0eb386a321210527c11842fae5aad970e841
Valentin Degenne <vdegenne@gmail.com>
2025-01-30 10:09:03 +0100

    back

---
+++ background.js
@@ -0,0 +1,17 @@
+chrome.commands.onCommand.addListener(async (command) => {
+  if (command === "toggle-play") {
+    // Get all open YouTube tabs
+    let tabs = await chrome.tabs.query({ url: "*://www.youtube.com/*" });
+
+    if (tabs.length > 0) {
+      // Find the last active YouTube tab
+      let lastTab = tabs[tabs.length - 1];
+
+      // Execute the content script on that tab
+      chrome.scripting.executeScript({
+        target: { tabId: lastTab.id },
+        files: ["content.js"],
+      });
+    }
+  }
+});
