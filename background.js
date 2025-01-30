d12f0e84aeadfc91ac5d62db7ea99f6ce1f80d37
Valentin Degenne <vdegenne@gmail.com>
2025-01-30 10:23:37 +0100

    script func

--- background.js
+++ background.js
@@ -1,17 +1,25 @@
 chrome.commands.onCommand.addListener(async (command) => {
-  if (command === "toggle-play") {
-    // Get all open YouTube tabs
-    let tabs = await chrome.tabs.query({ url: "*://www.youtube.com/*" });
+	if (command === 'toggle-play') {
+		// Get all open YouTube tabs
+		let tabs = await chrome.tabs.query({url: '*://www.youtube.com/*'});
 
-    if (tabs.length > 0) {
-      // Find the last active YouTube tab
-      let lastTab = tabs[tabs.length - 1];
+		if (tabs.length > 0) {
+			// Find the last active YouTube tab
+			let lastTab = tabs[tabs.length - 1];
 
-      // Execute the content script on that tab
-      chrome.scripting.executeScript({
-        target: { tabId: lastTab.id },
-        files: ["content.js"],
-      });
-    }
-  }
+			// Execute the content script on that tab
+			chrome.scripting.executeScript({
+				target: {tabId: lastTab.id},
+				// files: ["content.js"],
+				func: () => {
+					const video = document.querySelector('video');
+					if (video.paused) {
+						video.play();
+					} else {
+						video.pause();
+					}
+				},
+			});
+		}
+	}
 });
